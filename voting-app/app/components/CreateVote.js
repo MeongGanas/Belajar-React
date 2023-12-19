d03d196f6ff5e34cc4b38b08e70afcd8e8d575d5
MeongGanas <farreluken@gmail.com>
2023-12-19 18:25:12 +0800

    menambah fitur validasi form

--- voting-app/app/components/CreateVote.js
+++ voting-app/app/components/CreateVote.js
@@ -16,16 +16,27 @@ export default function CreateVote({ handleMake }) {
   const [open, setOpen] = useState(false);
   const [totalPilihan, setTotalPilihan] = useState(1);
 
-  const [title, setTitle] = useState("");
-  const [subtitle, setSubtitle] = useState("");
-  const [selections, setSelections] = useState([]);
-  const [category, setCategory] = useState("");
+  const [formData, setFormData] = useState({
+    title: { value: "", error: false },
+    subtitle: { value: "", error: false },
+    selections: { value: [], error: false },
+    category: { value: "", error: false },
+  });
+
+  const [error, setError] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
+    setError(false);
+    setFormData({
+      title: { value: "", error: false },
+      subtitle: { value: "", error: false },
+      selections: { value: [], error: false },
+      category: { value: "", error: false },
+    });
   };
 
   const handleInc = () => {
@@ -36,14 +47,39 @@ export default function CreateVote({ handleMake }) {
   };
 
   const handlePilihan = (e, index) => {
-    const nextPilihan = [...selections];
+    const nextPilihan = [...formData.selections.value];
     nextPilihan[index] = e.target.value;
-    setSelections(nextPilihan);
+    setFormData((prevData) => ({
+      ...prevData,
+      selections: { value: nextPilihan, error: false },
+    }));
   };
 
   const handleMakePolling = () => {
-    handleMake(title, subtitle, selections, category);
-    handleClose();
+    const { title, subtitle, selections, category } = formData;
+
+    const isTitleValid = title.value.length > 0;
+    const isSubtitleValid = subtitle.value.length > 0;
+    const isSelectionsValid = selections.value.length > 0;
+    const isCategoryValid = category.value.length > 0;
+
+    if (
+      isTitleValid &&
+      isSubtitleValid &&
+      isSelectionsValid &&
+      isCategoryValid
+    ) {
+      handleMake(title.value, subtitle.value, selections.value, category.value);
+      handleClose();
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        title: { ...prevData.title, error: !isTitleValid },
+        subtitle: { ...prevData.subtitle, error: !isSubtitleValid },
+        selections: { ...prevData.selections, error: !isSelectionsValid },
+        category: { ...prevData.category, error: !isCategoryValid },
+      }));
+    }
   };
 
   const renderPilihan = () => {
@@ -51,8 +87,10 @@ export default function CreateVote({ handleMake }) {
       <TextField
         id="title"
         label={`Pilihan ke ${index + 1}`}
+        required
         key={index}
         variant="outlined"
+        error={formData.selections.error}
         sx={{ mt: 3 }}
         fullWidth
         onChange={(e) => handlePilihan(e, index)}
@@ -79,17 +117,31 @@ export default function CreateVote({ handleMake }) {
             id="title"
             label="Judul Polling"
             variant="outlined"
+            required
             fullWidth
+            error={formData.title.error}
             sx={{ mt: 3 }}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) =>
+              setFormData((prevData) => ({
+                ...prevData,
+                title: { value: e.target.value, error: false },
+              }))
+            }
           />
           <TextField
             id="subtitle"
             label="Subtitle Polling"
+            required
             variant="outlined"
             sx={{ mt: 3 }}
+            error={formData.subtitle.error}
             fullWidth
-            onChange={(e) => setSubtitle(e.target.value)}
+            onChange={(e) =>
+              setFormData((prevData) => ({
+                ...prevData,
+                subtitle: { value: e.target.value, error: false },
+              }))
+            }
           />
 
           {renderPilihan()}
@@ -104,16 +156,31 @@ export default function CreateVote({ handleMake }) {
           <TextField
             id="category"
             label="Kategori"
+            required
             variant="outlined"
             sx={{ mt: 3 }}
             fullWidth
-            onChange={(e) => setCategory(e.target.value)}
+            error={formData.category.error}
+            onChange={(e) =>
+              setFormData((prevData) => ({
+                ...prevData,
+                category: { value: e.target.value, error: false },
+              }))
+            }
           />
+
+          {error ? (
+            <h1 className="text-red-700 mt-5">
+              Anda harus mengisi semua inputan!
+            </h1>
+          ) : (
+            ""
+          )}
 
           <div className="mt-5">
             <Button
               variant="contained"
-              sx={{ mr: 3 }}
+              sx={{ mr: 1 }}
               onClick={handleMakePolling}
             >
               Buat
